db2fe8152e161a7955760f1862e588808dcf49ef
dmroy <dmitriy.roy@ita.biz.ua>
2018-02-18 15:38:50 +0200

    mycommit

---
+++ src/FileSynch.java
@@ -0,0 +1,7 @@
+public class FileSynch {
+    public static void main(String[] args) {
+        if(args == null || args.length != 2){
+            System.err.println("Not enough arguments.");
+        }
+    }
+}
