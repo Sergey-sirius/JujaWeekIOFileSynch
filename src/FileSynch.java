1600d0facb396ca222e2d84e2ac1e3f4e70b989e
dmroy <dmitriy.roy@ita.biz.ua>
2018-02-18 17:53:49 +0200

    обработка аргументов

--- src/FileSynch.java
+++ src/FileSynch.java
@@ -2,6 +2,9 @@ public class FileSynch {
     public static void main(String[] args) {
         if(args == null || args.length != 2){
             System.err.println("Not enough arguments.");
+            System.exit(-1);
         }
+        System.out.println("args-1 : " + args[0]);
+        System.out.println("args-2 : " + args[1]);
     }
 }
