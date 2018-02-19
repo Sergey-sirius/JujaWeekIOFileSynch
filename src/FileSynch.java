c13b87527dcff6534353ebfa04610c1dd2525bfd
dmroy <dmitriy.roy@ita.biz.ua>
2018-02-19 06:38:27 +0200

    обработка аргументов

--- src/FileSynch.java
+++ src/FileSynch.java
@@ -1,10 +1,22 @@
+import java.io.File;
+
 public class FileSynch {
     public static void main(String[] args) {
         if(args == null || args.length != 2){
             System.err.println("Not enough arguments.");
             System.exit(-1);
         }
-        System.out.println("args-1 : " + args[0]);
-        System.out.println("args-2 : " + args[1]);
+        System.out.println("Source folder : " + args[0]);
+        System.out.println("Destination folder : " + args[1]);
+
+        File fileSource = new File(args[0]);
+        File fileDestination = new File(args[1]);
+
+        if(fileSource.exists()){
+            System.out.println("FilePath " + fileDestination + " exist");
+        }else{
+            System.out.println("FilePath " + fileDestination + " NOT exist");
+        }
+
     }
 }
