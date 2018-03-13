250b687f2c4ce2ad106887f012df9435cd931b66
dmroy <dmitriy.roy@ita.biz.ua>
2018-03-13 10:59:22 +0200

    перед переделыванием на удаление рекурсией

--- src/FileSynch.java
+++ src/FileSynch.java
@@ -9,56 +9,9 @@ public class FileSynch {
 
     public static void main(String[] args) throws IOException {
 
-//        File dest222 = new File("D:/!!!!!_dest/222/");
-//        File source222 = new File("D:/!!!!!_source/222");
-//        System.out.println("dest222.exists() = " + dest222.exists());
-//        System.out.println("dest222.length() = " + dest222.length());
-//        System.out.println("dest222.getTotalSpace() = " + dest222.getTotalSpace());
-//        System.out.println("dest222.canWrite() = " + dest222.canWrite());
-//        System.out.println("dest222.canRead() = " + dest222.canRead());
-//        System.out.println("source222.exists() = " + source222.exists());
-//        System.out.println("source222.length() = " + source222.length());
-//        System.out.println("source222.getTotalSpace() = " + source222.getTotalSpace());
-//        System.out.println("source222.canWrite() = " + source222.canWrite());
-//        System.out.println("source222.canRead() = " + source222.canRead());
-
-//        File testFile = new File("D:/!!!!!!_test");
-////        File testFile = new File("D:/!!!!!!_test/1/12_1/13_1/");
-//        deleteFolder(testFile.getAbsolutePath());
-//        boolean b1 = testFile.delete(); // не работает
-//        System.out.println("testFile.exists() = " + testFile.exists());
-//        System.out.println("b1 = " + b1);
-//        testFile.deleteOnExit();
-//        System.out.println("testFile.exists() = " + testFile.exists());
-//        testFile.exists();
-//        System.out.println("testFile.exists() = " + testFile.exists());
-////        System.out.println("testFile.exists() = " + testFile.exists());
-
-
-
-
         args = new String[]{"D:/!!!!!_source","D:/!!!!!_dest"};
 
-        if(args == null || args.length != 2){
-            System.err.println("Not enough arguments.");
-            System.exit(-1);
-        }
-
-        args[0] = args[0].trim().replace("/","\\");
-        args[1] = args[1].trim().replace("/","\\");
-
-        while(args[0].lastIndexOf("\\") == args[0].length() - 1){
-            args[0] = args[0].substring(0,args[0].length()-1);
-        }
-
-        while(args[1].lastIndexOf("\\") == args[1].length() - 1){
-            args[1] = args[1].substring(0,args[1].length()-1);
-        }
-
-        args[0] = args[0].lastIndexOf("\\") == args[0].length() + 1 ? args[0] : args[0] + "\\";
-        args[1] = args[1].lastIndexOf("\\") == args[1].length() + 1 ? args[1] : args[1] + "\\";
-
-        String[] finalArgs = args;
+        checkParams(args);
 
         System.out.println("Source folder : " + args[0]);
         System.out.println("Destination folder : " + args[1]);
@@ -90,37 +43,7 @@ public class FileSynch {
         });
 
         System.out.println("\n2---------------------------\n");
-        // обрабатывапем файлы
-        for (Map.Entry<String, Long> destinationFile : destList.entrySet()) {
-            String filePath = destinationFile.getKey();
-            File f = new File(args[1] + filePath);
-            if(f.isFile()) {
-                Long fileSize = destinationFile.getValue();
-                if (sourceList.containsKey(filePath) && sourceList.get(filePath).equals(fileSize)) {
-                    System.out.println(args[1] + filePath + " : удалить из списка источника - файл без изменений, удалять и копировать не надо.");
-                    sourceList.remove(filePath);
-                } else if (!sourceList.containsKey(filePath)) {
-                    System.out.println(args[1] + filePath + " : удалить файл");
 
-                }
-            }
-        }
-
-        System.out.println("\n3---------------------------\n");
-        // обрабатываем каталоги
-        for (Map.Entry<String, Long> destinationFile : destList.entrySet()) {
-            String filePath = destinationFile.getKey();
-            File directory = new File(args[1] + filePath);
-            if(directory.isDirectory()) {
-                if (sourceList.containsKey(filePath)) {
-                    System.out.println(filePath + " : удалить из списка источника - файл без изменений, удалять и копировать не надо.");
-                    sourceList.remove(filePath);
-                } else if (!sourceList.containsKey(filePath)) {
-                    System.out.println(args[1] + filePath + " : удалить каталог");
-//                    deleteFolder(args[1] + directory);
-                }
-            }
-        }
 
         System.out.println("\n4---------------------------\n");
 
@@ -128,51 +51,27 @@ public class FileSynch {
             System.out.println("sourceList file " + k + "; size = " + v);
         });
 
-//        sourceList.forEach((k, v) -> {
-//            copyFile(finalArgs[0],finalArgs[1], k);
-//            System.out.println("copy file " + (finalArgs[0] + k) + " into " + finalArgs[1]);
-//        });
+    }
 
+    private static void checkParams(String[] args) {
+        if(args == null || args.length != 2){
+            System.err.println("Not enough arguments.");
+            System.exit(-1);
+        }
 
+        args[0] = args[0].trim().replace("/","\\");
+        args[1] = args[1].trim().replace("/","\\");
 
-//        for (Map.Entry<String, Long> file : destList.entrySet()) {
-//            String filePath = file.getKey();
-//            Long fileSize = file.getValue();
-//            File f = new File(finalArgs[1] + filePath);
-//            if(!sourceList.containsKey(filePath)){
-//                if(f.isFile()){
-//                    System.out.println("delete file " + (finalArgs[1] + filePath) + "; " + f.delete());
-//                }else{
-//                    deleteFolder(finalArgs[1] + filePath);
-//                    System.out.println("delete folder " + (finalArgs[1] + filePath) + ";");
-//                }
-//            }else if(sourceList.containsKey(filePath) &&
-//                    sourceList.get(filePath).equals(fileSize) &&
-//                    f.isFile()){
-//                sourceList.remove(filePath);
-//            }else if(sourceList.containsKey(filePath) &&
-//                        (sourceList.containsKey(filePath) &&
-//                            !sourceList.get(filePath).equals(fileSize) &&
-//                            f.isFile())){
-//                File f1 = new File(finalArgs[1]+filePath);
-//                boolean isDelete = false;
-//                if(f1.isFile()){
-//                    isDelete = f1.delete();
-//                    System.out.println("delete file " + (finalArgs[1] + filePath) + "; " + isDelete);
-//                }else{
-//                    deleteFolder(finalArgs[1] + filePath);
-//                    System.out.println("delete folder " + (finalArgs[1] + filePath) + "; " + isDelete);
-//                }
-//            }
-//        }
+        while(args[0].lastIndexOf("\\") == args[0].length() - 1){
+            args[0] = args[0].substring(0,args[0].length()-1);
+        }
 
+        while(args[1].lastIndexOf("\\") == args[1].length() - 1){
+            args[1] = args[1].substring(0,args[1].length()-1);
+        }
 
-/*
-        sourceList.forEach((k, v) -> {
-            copyFile(finalArgs[0],finalArgs[1], k);
-            System.out.println("copy file " + (finalArgs[0] + k) + " into " + finalArgs[1]);
-        });
-*/
+        args[0] = args[0].lastIndexOf("\\") == args[0].length() + 1 ? args[0] : args[0] + "\\";
+        args[1] = args[1].lastIndexOf("\\") == args[1].length() + 1 ? args[1] : args[1] + "\\";
     }
 
     public static void deleteFolder(String path){
